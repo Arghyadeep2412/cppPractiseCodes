b6fd5ed4bd736a455b1c0a42322f836539202e76
osmarghyadeep <arghyadep.g@partner.import.io>
2021-01-18 12:08:17 +0530

    Need to update for counting set bits

--- bit-manipulation-codes/bitManipulations.cpp
+++ bit-manipulation-codes/bitManipulations.cpp
@@ -18,6 +18,53 @@ bool isKthBitSetRightShift(int n, int k) {
     return kthBitIsSet;
 }
 
+//---------------count the number of set bits---------------------
+int countSetBitsNaive(int n) {
+    int res = 0;
+    while(n > 0) {
+        // if((n % 2) != 0) {
+        //     res++;
+        // }
+        res = res + (n & 1);
+        n = (n/2);
+        // if((n & 1) == 1) {
+        //     res++;
+        // }
+        // n = (n >> 1);
+    }
+}
+
+int countSetBitsBrianKerningamAlgo(int n) {
+    // time complexity -- theta(count of set bits)
+    int res = 0;
+    while(n > 0) {
+        n = n & (n - 1);
+        res++;
+    }
+    return res;
+}
+
+int table[256];
+void initialize() {
+    table[0] = 0;
+    for(int i = 1; i < 256; i++) {
+        table[i] = table[i/2] + (i & 1);
+    }
+}
+
+int count(int n) {
+    int res = 0;
+    initialize();
+    res = res + table[n & 0xff];
+    n = n >> 8;
+    res = res + table[n & 0xff];
+    n = n >> 8;
+    res = res + table[n & 0xff];
+    n = n >> 8;
+    res = res + table[n & 0xff];
+    return res;
+}
+
 int main() {
     return 0;
 }

2ded09828476a2248ef88f865405f4523ac8e7d4
osmarghyadeep <arghyadep.g@partner.import.io>
2021-01-20 14:23:46 +0530

    completed theory of bit wise manip snippets

--- bit-manipulation-codes/bitManipulations.cpp
+++ bit-manipulation-codes/bitManipulations.cpp
@@ -95,7 +95,7 @@ bool checkPowOf2BrianKerningham (int n) {
     return isPowerOf2;
 }
 
-// ------------need to find a number which is occuring odd number of times (only one in the given array)---------------------
+//-------------need to find a number which is occuring odd number of times (only one in the given array)---------------------
 int findOddOccuringNum(int arr[], int length) {
     int n = arr[0];
     for(int i = 1; i < length; i++) {
@@ -103,6 +103,59 @@ int findOddOccuringNum(int arr[], int length) {
     }
     return n;
 }
+
+//-----------------------given a array of integers---------
+// all of them appear even number of times
+// only two of them appear odd number of times
+// need to find the odd occuring elements -- input array size is atleast 2
+#include<vector>
+vector<int> listOddOccuringElms(int arr[], int n) {
+    vector<int> res;
+    int res1 = 0;
+    int res2 = 0;
+    int xor = 0;
+    for(int i = 0; i < n; i++) {
+        xor = xor ^ arr[i];
+    }
+    int rightMostSetBit = xor & (~(xor - 1)); // a num which has the only set bit as the last set bit in xor
+    for(int i = 0; i < n; i++) {
+        if((arr[i] & rightMostSetBit) != 0) {
+            res1 = res1 ^ arr[i];
+        } else {
+            res2 = res2 ^ arr[i];
+        }
+    }
+    res.push_back(res1);
+    res.push_back(res2);
+    return res;
+}
+
+//--------------------create subsets of the given string---------------------------
+// string has - n chars -- there are pow(2, n) subsets in all
+// time complexity -- O(n * (2 ^ n))
+#include<vector>
+#include<string>
+#include<cmath>
+vector<string> getPowerSet(string str) {
+    vector<string> res;
+    if(str.empty()) {
+        return res;
+    }
+    int len = str.length();
+    string thisStr = "";
+    int powSetLen = pow(2, len);
+    for(int i = 0; i < powSetLen; i++) {
+        thisStr = "";
+        for(int j = 0; j < len; j++) {
+            if((i & (1 << j)) != 0) {
+                thisStr = thisStr + str[j];
+            }
+        }
+        res.push_back(thisStr);
+    }
+    return res;
+}
+
 
 int main() {
     return 0;
